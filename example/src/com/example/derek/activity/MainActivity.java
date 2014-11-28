853d4cb269bffa457365a9db16070ad16b617a7f
liudingyu <david_katizer@163.com>
2014-11-28 11:21:27 +0800

    add getversioncode function
    Change-Id: Ib6980ad3871d2502c7c99dc89f9a8b166d43a59b

--- example/src/com/example/derek/activity/MainActivity.java
+++ example/src/com/example/derek/activity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.derek.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import com.example.derek.R;
+import com.example.derek.utils.AppUtils;
 
 public class MainActivity extends Activity {
     /**
@@ -11,7 +14,14 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weight_test);
+        setContentView(R.layout.main);
+        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int versioncode = AppUtils.getVersionCode(MainActivity.this, "");
+                Toast.makeText(MainActivity.this,"" + versioncode, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onDestroy() {
