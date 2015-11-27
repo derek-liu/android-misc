17d8d5bb32ff52035d506ba2db88ba8aed7661c6
derek <derek.liu0221@gmail.com>
2015-11-28 00:24:03 +0800

    test change locale

---
+++ example/src/com/example/derek/activity/LanguageActivity.java
@@ -0,0 +1,14 @@
+package com.example.derek.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by liudingyu on 11/27/15.
+ */
+public class LanguageActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
