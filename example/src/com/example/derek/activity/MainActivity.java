3d0654242fb3a55a6d86b3524a082f4bdc870e07
liudingyu <david_katizer@163.com>
2015-11-29 11:41:44 +0800

    modify code
    Change-Id: I56c3bb03b4b23330e279b3dac883f17926c9e9f8

--- example/src/com/example/derek/activity/MainActivity.java
+++ example/src/com/example/derek/activity/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.derek.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.text.ICUCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import com.example.derek.R;
+import org.json.JSONObject;
+
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -19,7 +24,8 @@ public class MainActivity extends Activity {
         mBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                handleClick();
+//                handleClick();
+                printLocaleInfo();
             }
         });
     }
@@ -27,5 +33,20 @@ public class MainActivity extends Activity {
     private void handleClick() {
         Intent intent = new Intent(this, LanguageActivity.class);
         startActivity(intent);
+    }
+
+    private void printLocaleInfo() {
+        try {
+            JSONObject obj = new JSONObject();
+            obj.put("getCountry", Locale.getDefault().getCountry());
+            obj.put("getDisplayCountry", Locale.getDefault().getDisplayCountry());
+            obj.put("getDisplayLanguage", Locale.getDefault().getDisplayLanguage());
+            obj.put("getLanguage", Locale.getDefault().getLanguage());
+            obj.put("getISO3Country", Locale.getDefault().getISO3Country());
+            obj.put("getISO3Language", Locale.getDefault().getISO3Language());
+            obj.put("maximizeAndGetScriptt", ICUCompat.maximizeAndGetScript(Locale.getDefault()));
+            Log.d("d.d", obj.toString());
+        } catch (Exception e) {
+        }
     }
 }
